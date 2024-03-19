ec087fe0b97dfde3f09e0967f3bd5c05ae15f9a1
shiiqcx <149090282+shiiqcx@users.noreply.github.com>
2024-03-19 15:23:09 +0000

    review

--- review.js
+++ review.js
@@ -3,11 +3,16 @@
 
 // 2. Take look at the HMTL. When the user clicks the "option-one" button, we want the "option-one-screen" div to show. 
 //  - First, store those elements as variables. Can someone take me through the steps to do that?
+let optionOneButton = document.querySelector(".option-one")
+
+let optionOneScreen = document.querySelector(".option-one-screen")
 
 
 // CODE SOLO, PART 1
 // 3. Declare a variable to save the button with the class "option-two"
+let optionTwoButton = document.querySelector (".option-two")
 // 4. Declare a variable to save the div with the class "option-two-screen"
+let optionTwoScreen = document.querySelector(".option-two-screen")
 
 
 
@@ -27,14 +32,13 @@
 // 7. Plug in the variables we created to make it so that parts of the story show up when the user clicks the button! 
 //  - Uncomment the code, try it out on the webpage, and see what happens!
 
-/* Do not worry about the code below until the end! 
 
- INSERT VARIABLE FOR OPTION ONE BUTTON.addEventListener("click", function(){
-    INSERT VARIABLE FOR OPTION-ONE-SCREEN DIV.style.display="block";
+
+ optionOneButton.addEventListener("click", function(){
+    optionOneScreen.style.display="block";
 });
 
- INSERT VARIABLE FOR OPTION TWO BUTTON.addEventListener("click", function(){
-    INSERT VARIABLE FOR OPTION-TWO-SCREEN DIV.style.display="block";
+ optionTwoButton.addEventListener("click", function(){
+    optionTwoScreen.style.display="block";
 });
 
-*/
